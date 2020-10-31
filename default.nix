c083d585b32689d7c740b1f62bb1b89c27ac6f5e
diamondburned <datutbrus@gmail.com>
2020-10-30 17:26:26 -0700

    nix: skip tests to avoid ffmpeg deps

--- default.nix
+++ default.nix
@@ -6,6 +6,9 @@ pkgs.buildGoModule {
 
 	src = ./.;
 
+	# Skip the ffmpeg dependency.
+	doCheck = false;
+
 	CGO_ENABLED = "0";
 
 	vendorSha256 = "0lyxkf2yi5pg68f0462f7ar1m4vsaal09cfbmz3fkhwdzj221i6c";
